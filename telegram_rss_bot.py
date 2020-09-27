a46b928d6f8333cec80246c2062767b053ab1bc8
Amar0g <18593496+Amar0g@users.noreply.github.com>
2020-09-27 12:06:10 +0200

    Update telegram_rss_bot.py
    remove tests

--- telegram_rss_bot.py
+++ telegram_rss_bot.py
@@ -38,12 +38,9 @@ def send_message(message):
 
 #parsing rss
 def main():
-    #rss_feed = feedparser.parse(RSS_URLS)
     currenttime = strftime("%Y-%m-%d %H:%M:%S", gmtime())   # get current time and format it with "strftime" method. 
                                                             # # The strftime() method returns a string representing date and time using date, time or datetime object.
     parsecurrenttime = parse(currenttime)
-    # print(parsecurrenttime)
-    # print(type(parsecurrenttime))
 
     for url in RSS_URLS:
         feeds.append(feedparser.parse(url))
@@ -54,15 +51,8 @@ def main():
                                                                                     # The strftime() method returns a string representing date and time using date, time or datetime object.
             parsepublishedtime = parse(publishedtime)
             deltatime = parsecurrenttime - parsepublishedtime
-            # delta_in_seconds = deltatime.total_seconds()
-            # print("delta in seconds : ")
-            # print(delta_in_seconds)
             
             if (deltatime.total_seconds() < ENTRY_MAX_TIME_OLD):
-                # print(parsepublishedtime)
-                # print(type(parsepublishedtime))
-                # print("Deltatime.seconds : " + str(deltatime.seconds))
-                # print("Deltatime.total_seconds() : " + str(deltatime.total_seconds()))
                 print("entry title: " + entry.title)
                 print("entry link: " + entry.link)
                 send_message(entry.title + "   " + entry.link)
